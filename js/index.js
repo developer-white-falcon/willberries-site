7ef793210ed046742452e2aa08d66568192075a7
developer-white-falcon <111103876+developer-white-falcon@users.noreply.github.com>
2022-12-02 07:41:39 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button-next',
+    prevEl: '.slider-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
